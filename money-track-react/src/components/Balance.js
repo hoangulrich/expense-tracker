0cc0420df3b3018657258e49fb7e82328990c38f
Hoang Le <artstyle0416@gmail.com>
2021-06-24 20:51:35 +0700

    frontend done

---
+++ money-track-react/src/components/Balance.js
@@ -0,0 +1,18 @@
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+
+const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amount = transactions.map((transaction) => transaction.amount);
+  const total = amount.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>${total}</h1>
+    </>
+  );
+};
+
+export default Balance;
